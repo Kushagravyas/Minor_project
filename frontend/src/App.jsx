d1d1068c97c82cf66f10c0e1445debf9233eed87
kushagra vyas <kushagravyas1122@gmail.com>
2025-05-13 00:39:54 +0530

    framer motion added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Markdown from "react-markdown"
 import rehypeHighlight from "rehype-highlight"
 import "highlight.js/styles/github-dark.css"
 import axios from "axios"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion, AnimatePresence } from "motion/react"
 import "./App.css"
 
 function App() {
